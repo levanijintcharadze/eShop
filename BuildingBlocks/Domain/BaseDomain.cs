bf5f739e2e11fa2c418501bb5cbe0c449da8f30c
Levani jintcharadze <levanijincharadze@outlook.com>
2020-08-28 18:48:48 +0400

    Add project files.

---
+++ BuildingBlocks/Domain/BaseDomain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
+using MediatR;
+
+namespace Domain
+{
+    public interface IDomainEvent : INotification
+    {
+        DateTime CreatedAt { get; }
+    }
+
+    public interface IDomainEventContext
+    {
+        IEnumerable<BaseDomain> GetDomainEvents();
+    }
+
+    public abstract class BaseDomain : IDomainEvent
+    {
+        public DateTime CreatedAt { get; }
+    }
+}
